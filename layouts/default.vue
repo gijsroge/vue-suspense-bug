bf5a096ed794b174495cc295e4d5920985bf0d70
gijs <gijs.roge@intracto.com>
2022-02-02 00:06:48 +0100

    Improve testing case

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,16 @@
 
 <template>
   <div>
-    <p>Try switching between route 2 and 3 very quickly to cause the bug. Route 1 is unaffected as it has no async data to wait for.</p>
+    <p>Switching quickly between these 2 routes works just fines</p>
     <ul>
       <li><router-link to="/"><strong>1</strong> go to page without async data and with normal component</router-link></li>
+      <li>
+        <router-link to="/3"><strong>3</strong> go to page with async data and async component</router-link>
+      </li>
+    </ul>
+
+    <p>Switching quickly between these 2 routes causes a Suspense issue</p>
+    <ul>
       <li><router-link to="/2"><strong>2</strong> go to page with async data and normal component</router-link></li>
       <li>
         <router-link to="/3"><strong>3</strong> go to page with async data and async component</router-link>
