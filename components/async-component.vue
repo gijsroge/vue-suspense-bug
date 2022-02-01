a4673f25f315aebf7882c6898ec4b0dcbb5735f5
gijs <gijs.roge@intracto.com>
2022-02-02 00:03:55 +0100

    reduce testing scope

--- components/async-component.vue
+++ components/async-component.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import { useFetch } from "#app";
 
-await new Promise(resolve => setTimeout(() => resolve(true), 1000))
 const { data } = await useFetch("https://jsonplaceholder.typicode.com/users");
 </script>
 <template>
